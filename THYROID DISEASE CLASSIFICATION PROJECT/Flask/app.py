3da3028ed5d7a43af133d0cc330dd7adec77bd6f
SRIMATHIMANIKKAVASAKAN <127217550+SRIMATHIMANIKKAVASAKAN@users.noreply.github.com>
2023-04-18 19:47:27 +0530

    Add files via upload

---
+++ THYROID DISEASE CLASSIFICATION PROJECT/Flask/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pickle
+import pandas as pd
+
+model = pickle.load(open(r"C:\\Users\LENOVO\\Downloads\\thyroid_1_model (1).pkl", 'rb'))
+le = pickle.load(open("C:\\Users\\LENOVO\\Downloads\\label_encoder.pkl", 'rb'))
+
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def about():
+      return render_template('home.html')
+
+
+
+@app.route("/pred", methods=['POST', 'GET'])
+def predict():
+    x = [[float(x) for x in request.form.values()]]
+
+
+    print(x)
+    col = ['goitre','tumor','hypopituitary','psych','TSH','T3','TT4','T4U','FTI','TBG']
+    x = pd.DataFrame(x, columns=col)
+
+    #print(x.shape)
+
+    print(x)
+    pred = model.predict(x)
+    pred = le.inverse_transform(pred)
+    print(pred[0])
+    return render_template('submit.html', prediction_text=str(pred))
+
+
+
+
+if __name__== "__main__":
+  app.run(debug=False)
